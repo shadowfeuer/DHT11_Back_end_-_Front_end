7fdcbccc5c3af99286b582db6b67af0f03a80168
shadowfeuer <93450344+shadowfeuer@users.noreply.github.com>
2025-07-01 13:18:28 -0400

    Update temp_and_hum_recorder.js

--- src/temp_and_hum_recorder.js
+++ src/temp_and_hum_recorder.js
@@ -3,23 +3,16 @@ var express = require('express');
 
 var date_time = new Date();
 
-var captor = {
-    {
-      type: 11,
-      pin: 17
-    },
-  read: getData() {
-      var data = sensorLib.read(
-        captor.type,
-        captor.pin
-      );
+var captor = 
+{
+    {type: 11,pin: 17},
+  
+read: getData() {
+      var data = sensorLib.read(captor.type,captor.pin);
       console.log(date_time +` temperature: ${data.temperature.toFixed(1)}°C, ` +`humidity: ${data.humidity.toFixed(1)}%`);
     }
-    
-setTimeout(getData() {
-      captor.read();
-    }, 2000);
-  }
+setTimeout(getData() {captor.read();}, 2000);
+}
 };
 
 captor.read();
